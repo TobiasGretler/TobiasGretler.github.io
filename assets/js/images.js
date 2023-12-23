6ca44af75e026fb473f44efd6f0d515f5662a5e3
TobiasGretler <txnitxbios@gmail.com>
2023-12-23 15:19:27 +0100

    bilderscript

--- assets/js/images.js
+++ assets/js/images.js
@@ -1,53 +1,242 @@
-const imageGalleryData = [
+const architecture = [
 	{
-		src:"assets/img/gallery/archi.jpg"
+		src:"assets/img/Architecture/cathedral-1066314_1280.jpg"
 	},
 	{
-		src:"assets/img/gallery/gallery-1.jpg"
+		src:"assets/img/Architecture/house-1867187_1280.jpg"
 	},
 	{
-		src:"assets/img/gallery/gallery-10.jpg"
+		src:"assets/img/Architecture/abstract-1138967_1280.jpg"
 	},
 	{
-		src:"assets/img/gallery/gallery-11.jpg"
+		src:"assets/img/Architecture/Bild3.jpeg"
+	},
+	{
+		src:"assets/img/Architecture/Bild2.jpeg"
+	},
+	{
+		src:"assets/img/Architecture/Bild4.jpeg"
+	},
+	{
+		src:"assets/img/Architecture/Bild.jpeg"
+	},
+	{
+		src:"assets/img/Architecture/architecture-2083687_1280.jpg"
+	},
+	{
+		src:"assets/img/Architecture/building-1727807_1920.jpg"
+	}
+];
+
+const travel = [
+	{
+		src:"assets/img/gallery/gallery-9.jpg"
 	},
 	{
 		src:"assets/img/gallery/gallery-12.jpg"
 	},
 	{
-		src:"assets/img/gallery/gallery-13.jpg"
+		src:"assets/img/gallery/gallery-8.jpg"
 	},
 	{
-		src:"assets/img/gallery/gallery-14.jpg"
-	},
-	{
-		src:"assets/img/gallery/gallery-15.jpg"
+		src:"assets/img/gallery/archi.jpg"
 	},
 	{
 		src:"assets/img/gallery/gallery-16.jpg"
 	},
 	{
-		src:"assets/img/gallery/gallery-2.jpg"
-	},
-	{
-		src:"assets/img/gallery/gallery-3.jpg"
-	},
-	{
-		src:"assets/img/gallery/gallery-4.jpg"
-	},
-	{
-		src:"assets/img/gallery/gallery-5.jpg"
-	},
-	{
-		src:"assets/img/gallery/gallery-6.jpg"
+		src:"assets/img/gallery/gallery-14.jpg"
 	},
 	{
 		src:"assets/img/gallery/gallery-7.jpg"
 	},
 	{
+		src:"assets/img/gallery/gallery-11.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-3.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-5.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-13.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-10.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-2.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-4.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-15.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-1.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-6.jpg"
+	}
+];
+const sport = [
+	{
+		src:"assets/img/gallery/gallery-9.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-12.jpg"
+	},
+	{
 		src:"assets/img/gallery/gallery-8.jpg"
 	},
 	{
+		src:"assets/img/gallery/archi.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-16.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-14.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-7.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-11.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-3.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-5.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-13.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-10.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-2.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-4.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-15.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-1.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-6.jpg"
+	}
+];
+const animal = [
+	{
 		src:"assets/img/gallery/gallery-9.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-12.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-8.jpg"
+	},
+	{
+		src:"assets/img/gallery/archi.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-16.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-14.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-7.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-11.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-3.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-5.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-13.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-10.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-2.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-4.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-15.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-1.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-6.jpg"
+	}
+];
+const nature = [
+	{
+		src:"assets/img/gallery/gallery-9.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-12.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-8.jpg"
+	},
+	{
+		src:"assets/img/gallery/archi.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-16.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-14.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-7.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-11.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-3.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-5.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-13.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-10.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-2.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-4.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-15.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-1.jpg"
+	},
+	{
+		src:"assets/img/gallery/gallery-6.jpg"
 	}
 ];
